ac7df5589fce04a673ab6e8b878514bf7a76f6fb
Pavan S <iampavan05@gmail.com>
2023-04-13 09:35:53 +0530

    ✨ init setup > keypress.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-function App() {
+import { useEffect } from "react";
+
+function App({ listener }) {
+  useEffect(() => {
+    listener.simple_combo("shift s", function () {
+      console.log("You pressed shift and s");
+    });
+  }, [listener]);
   return <div className="App">Hello React</div>;
 }
 
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+const listener = new window.keypress.Listener();
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<App listener={listener} />);
