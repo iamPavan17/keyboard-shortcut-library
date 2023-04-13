a057dfe764d442b55af9626de85bfea4e0d4e274
Pavan S <iampavan05@gmail.com>
2023-04-13 12:40:54 +0530

    🐛 fix: className issue

--- src/components/KeyboardShortcutLegend.js
+++ src/components/KeyboardShortcutLegend.js
@@ -8,7 +8,7 @@ export default function KeyboardShortcutLegend({
     <div className="component-wrapper keyboard-legend">
       {keyboardCombos.map((item) => (
         <div key={item.id} className="keyboard-combo-list">
-          <p className={!item.isActive && "unbind-text"}>
+          <p className={!item.isActive ? "unbind-text" : ""}>
             {item.keyboardCombo.split(" ").join("-")}({item.description})
           </p>
           <button onClick={() => handleKeyboardCombo(item.id, "bindToggle")}>
