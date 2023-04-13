0b5c632d5591e7d81ea313a11dae4364f8f8c6c2
Pavan S <iampavan05@gmail.com>
2023-04-13 10:01:33 +0530

    ✨ added components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import { useEffect } from "react";
 
+import ComponentA from "./components/ComponentA";
+import ComponentB from "./components/ComponentB";
+import ComponentC from "./components/ComponentC";
+import KeyboardShortcutLegend from "./components/KeyboardShortcutLegend";
+
 function App({ listener }) {
   useEffect(() => {
     listener.simple_combo("shift s", function () {
       console.log("You pressed shift and s");
     });
   }, [listener]);
-  return <div className="App">Hello React</div>;
+
+  return (
+    <div className="container">
+      <ComponentA />
+      <ComponentB />
+      <ComponentC />
+      <KeyboardShortcutLegend />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/KeyboardShortcutLegend.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function KeyboardShortcutLegend() {
+  return <div className="component-wrapper">KeyboardShortcutLegend</div>;
+}
